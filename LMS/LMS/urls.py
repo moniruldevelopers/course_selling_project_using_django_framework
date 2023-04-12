13c9b4712e6e248c27186eb5bd9ac10e5dee1fac
moniruldevelopers <monirul.developers@gmail.com>
2023-04-12 21:37:52 +0600

    1st signup create

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -5,11 +5,16 @@ from . import views,user_login
 urlpatterns = (
     path('admin/', admin.site.urls),
     path('base', views.BASE, name='base'),
+
     path('', views.HOME, name='home'),
     path('single/course', views.SINGLE_COURSE, name='single_course'),
+
     path('contact', views.CONTACT_US, name='contact_us'),
     path('about', views.ABOUT_US, name='about_us'),
-    path('accouts/register', user_login.REGISTER, name= ' register'),
+
+    path('accouts/register', user_login.REGISTER, name= 'register'),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('doLogin', user_login.DO_LOGIN, name='doLogin'),
 
 )
--- LMS/LMS/user_login.py
+++ LMS/LMS/user_login.py
@@ -1,5 +1,34 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def REGISTER(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # check email
+        if User.objects.filter(email=email).exists():
+            messages.warning(request, 'Email are Already Exists !')
+            return redirect('register')
+
+        # check username
+        if User.objects.filter(username=username).exists():
+            messages.warning(request, 'Username are Already exists !')
+            return redirect('register')
+
+        user = User(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+        return redirect('login')
+
     return render(request, 'registration/register.html')
+
+
+def DO_LOGIN():
+    return None
