f96aa3c66b6cf5f0903f383ad5b17fb569af2f31
moniruldevelopers <monirul.developers@gmail.com>
2023-05-07 15:31:31 +0600

    class 21 update ss

--- LMS/app/admin.py
+++ LMS/app/admin.py
@@ -11,8 +11,12 @@ class Requirements_TabularInline(admin.TabularInline):
     model = Requirements
 
 
+class Video_TabularInline(admin.TabularInline):
+    model = Video
+
+
 class course_admin(admin.ModelAdmin):
-    inlines = (What_you_learn_TabularInline, Requirements_TabularInline)
+    inlines = (What_you_learn_TabularInline, Requirements_TabularInline, Video_TabularInline)
 
 
 admin.site.register(Categories)
@@ -21,3 +25,4 @@ admin.site.register(Course, course_admin)
 admin.site.register(Level)
 admin.site.register(What_you_learn)
 admin.site.register(Requirements)
+admin.site.register(Lesson)
